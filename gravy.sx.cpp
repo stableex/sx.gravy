79ace0bd6d7e25e805295ccc5f7ae6e6a6a4faf5
Denis <carriere.denis@gmail.com>
2020-10-30 23:31:17 -0400

    update gravy

--- gravy.sx.cpp
+++ gravy.sx.cpp
@@ -1,26 +1,30 @@
 #include <eosio.token/eosio.token.hpp>
 #include "gravy.sx.hpp"
 
+void sx::gravy::reset()
+{
+    require_auth( get_self() );
+    sx::gravy::state _state( get_self(), get_self().value );
+    _state.remove();
+}
+
 [[eosio::on_notify("gravyhftdefi::transfer")]]
 void sx::gravy::on_gravy( const name from, const name to, const asset quantity, const string memo )
 {
     const name contract = "gravyhftdefi"_n;
     const symbol_code symcode = symbol_code{"GRV"};
 
-    // only mine once per block
-    // sx::gravy::state _state( get_self(), get_self().value );
-    // auto state = _state.get_or_default();
-    // if (_state.exists()) _state.remove();
+    // record state
+    sx::gravy::state _state( get_self(), get_self().value );
+    auto state = _state.get_or_default();
+    state.last = current_time_point();
+    state.count += 1;
+    state.total += quantity;
+    _state.set(state, get_self());
 
     // const time_point last = current_time_point();
     // check(state.last != last, "can only mine 1 block per transaction");
     check(quantity.amount > 30000, "must be above 30000 GRV");
-
-    // // update state
-    // state.last = last;
-    // state.count += 1;
-    // state.total += quantity;
-    // if (_state.set(state, get_self());
 
     // empty GRV
     const asset balance = eosio::token::get_balance( contract, get_self(), symbol_code{"GRV"});
@@ -28,6 +32,7 @@ void sx::gravy::on_gravy( const name from, const name to, const asset quantity, 
         eosio::token::transfer_action transfer( contract, { get_self(), "active"_n });
         transfer.send(get_self(), "swap.defi"_n, balance, "swap,0,786");
     }
+
     // empty EOS
     const asset balance_eos = eosio::token::get_balance( "eosio.token"_n, get_self(), symbol_code{"EOS"});
     if ( balance_eos.amount > 0 ) {
--- gravy.sx.hpp
+++ gravy.sx.hpp
@@ -21,12 +21,15 @@ class [[eosio::contract("gravy.sx")]] gravy : public eosio::contract {
 public:
     using contract::contract;
 
-    // struct [[eosio::table("state")]] state_row {
-    //     time_point      last;
-    //     uint64_t        count = 0;
-    //     asset           total = asset{0, symbol{"GRV", 8}};
-    // };
-    // typedef eosio::singleton< "state"_n, state_row > state;
+    struct [[eosio::table("state")]] state_row {
+        time_point      last;
+        uint64_t        count = 0;
+        asset           total = asset{0, symbol{"GRV", 8}};
+    };
+    typedef eosio::singleton< "state"_n, state_row > state;
+
+    [[eosio::action]]
+    void reset();
 
     [[eosio::on_notify("gravyhftdefi::transfer")]]
     void on_gravy( const name from, const name to, const asset quantity, const string memo );
